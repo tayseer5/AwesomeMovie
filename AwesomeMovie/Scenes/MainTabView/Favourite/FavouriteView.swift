e783daea964b055d3d6ca24e6c93eb8ee9655c1f
tayseer5 <tayseer.anwar92@gmail.com>
2023-02-09 11:19:37 +0200

    Favourite view
    handling favuorite view controller and it's view

---
+++ AwesomeMovie/Scenes/MainTabView/Favourite/FavouriteView.swift
@@ -0,0 +1,11 @@
+//
+//  FavouriteView.swift
+//  AwesomeMovie
+//
+//  Created by tayseer anwar on 09/02/2023.
+//
+
+import Foundation
+
+protocol FavouriteView: BaseView{
+}
---
+++ AwesomeMovie/Scenes/MainTabView/Favourite/FavouriteViewController.swift
@@ -0,0 +1,31 @@
+//
+//  FavouriteViewController.swift
+//  AwesomeMovie
+//
+//  Created by tayseer anwar on 09/02/2023.
+//
+
+import UIKit
+
+class FavouriteViewController: UIViewController, FavouriteView {
+    var finishFlow: (() -> Void)?
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
