68ca1bf7cf9c36ef05c58a91ec7dfa1989beb819
tayseer5 <tayseer.anwar92@gmail.com>
2023-02-09 11:22:35 +0200

    coordinator creatour
    handling coordinator creatour for home , fav and tab Modules

--- AwesomeMovie/Scenes/Coordinators/CoordinatorsFactory/CoordinatorFactory.swift
+++ AwesomeMovie/Scenes/Coordinators/CoordinatorsFactory/CoordinatorFactory.swift
@@ -6,6 +6,10 @@
 //
 
 import Foundation
+import UIKit
 
 protocol CoordinatorFactory {
+    func makeTabCoordiantor() -> (Coordinator & TabCoordinatorOutput, toPresent: Presentable?)
+    func makeHomeCoordinator(navController: UINavigationController?) -> Coordinator & HomeCoordinatorOutput
+    func makeFavouriteCoordinator(navController: UINavigationController?) -> Coordinator & FavouriteCoordinatorOutput
 }
