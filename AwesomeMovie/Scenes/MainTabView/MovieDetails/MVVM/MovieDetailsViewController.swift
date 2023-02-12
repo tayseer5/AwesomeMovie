fa04b06d0a45861122d2cb372aaf1effc7e40c3a
tayseer5 <tayseer.anwar92@gmail.com>
2023-02-13 01:25:53 +0200

    bind details design

--- AwesomeMovie/Scenes/MainTabView/MovieDetails/MVVM/MovieDetailsViewController.swift
+++ AwesomeMovie/Scenes/MainTabView/MovieDetails/MVVM/MovieDetailsViewController.swift
@@ -12,8 +12,12 @@ class MovieDetailsViewController: UIViewController,MovieDetailsView {
     @IBOutlet weak var DetailsTableView: UITableView! {
         didSet {
             DetailsTableView.registerNib(PosterImageTableViewCell.self)
+            DetailsTableView.registerNib(BasicInfoTableViewCell.self)
+            DetailsTableView.registerNib(DetailsTableViewCell.self)
+            DetailsTableView.estimatedRowHeight = 100.0;
             DetailsTableView.delegate = self
             DetailsTableView.dataSource = self
+            DetailsTableView.backgroundColor = UIColor(hexString: HexColors.grayshGreen.rawValue)
         }
     }
     // MARK: var
@@ -22,7 +26,10 @@ class MovieDetailsViewController: UIViewController,MovieDetailsView {
     //MARK: Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.tabBarController?.tabBar.isHidden = true
+        let backButton = UIBarButtonItem()
+        backButton.title = ""
+        self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
+        self.navigationController?.navigationBar.tintColor = .white
         // Do any additional setup after loading the view.
     }
     
@@ -40,24 +47,55 @@ class MovieDetailsViewController: UIViewController,MovieDetailsView {
 //MARK: - UITableViewDelegate Extention
 extension MovieDetailsViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 250
+        switch indexPath.row {
+        case detailsOrder.poseterImage.rawValue:
+            return 250
+        case detailsOrder.basicInfo.rawValue:
+            return 100
+        case detailsOrder.details.rawValue:
+            return UITableView.automaticDimension
+        default:
+            return UITableView.automaticDimension
+        }
+        
     }
 }
 //MARK: - UITableViewDataSource Extention
 extension MovieDetailsViewController: UITableViewDataSource {
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 3
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let data = viewModel?.getData() else {
             return UITableViewCell ()
         }
-        let cell = tableView.dequeue(PosterImageTableViewCell.self, indexPath: indexPath)
-        cell.configure(data: data, indexPath: indexPath)
-        return cell
+        switch indexPath.row {
+        case detailsOrder.poseterImage.rawValue:
+            let cell = tableView.dequeue(PosterImageTableViewCell.self, indexPath: indexPath)
+            cell.configure(data: data, indexPath: indexPath)
+            return cell
+        case detailsOrder.basicInfo.rawValue:
+            let cell = tableView.dequeue(BasicInfoTableViewCell.self, indexPath: indexPath)
+            cell.configure(data: data, indexPath: indexPath)
+            return cell
+        case detailsOrder.details.rawValue:
+            let cell = tableView.dequeue(DetailsTableViewCell.self, indexPath: indexPath)
+            cell.configure(data: data, indexPath: indexPath)
+            return cell
+        default:
+            return UITableViewCell ()
+        }
+    }
+    func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        cell.layoutIfNeeded()
     }
     
-    
+}
+
+enum detailsOrder: Int {
+    case poseterImage = 0
+    case basicInfo = 1
+    case details  = 2
 }
